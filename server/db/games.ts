739aab4fba76aa9654600a23066699ca2954c454
kurt <liangke1991@yahoo.com>
2023-04-15 11:41:22 +1200

    fix game DB function select

--- server/db/games.ts
+++ server/db/games.ts
@@ -2,9 +2,25 @@ import connection from './connection'
 import type { Game } from '../../models/Game'
 
 export function getGames(db = connection): Promise<Game[]> {
-  return db('games').select()
+  return db('games').select(
+    'id',
+    'name',
+    'description',
+    'number_player as playerCount',
+    'play_time as averagePlayTime',
+    'photo_url as photoUrl'
+  )
 }
 
 export function getGamesById(id: number, db = connection): Promise<Game[]> {
-  return db('games').select().where('id', id)
+  return db('games')
+    .select(
+      'id',
+      'name',
+      'description',
+      'number_player as playerCount',
+      'play_time as averagePlayTime',
+      'photo_url as photoUrl'
+    )
+    .where('id', id)
 }
