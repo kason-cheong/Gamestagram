c859e859cea6d154c8b569f649b58998e9e74f84
kason cheong <kasonck@yahoo.com>
2023-04-18 14:43:38 +1200

    Made the host clickable as well lol

--- client/components/EventDetail.tsx
+++ client/components/EventDetail.tsx
@@ -127,14 +127,16 @@ function EventDetail() {
             key={host.username}
             className=" group text-right sm:h-32 h-14 mt-10 group relative duration-300"
           >
-            <img
-              className="object-center inline-block w-2/3 h-1/2 rounded-full ring-2 ring-white"
-              src={`${host.photoUrl}`}
-              alt={`${host.username}`}
-            />
-            <span className="absolute hidden group-hover:flex -right-1 -top-2 -translate-y-full  px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
-              {host.username}
-            </span>
+            <Link to={`/users/${event.hostId}`}>
+              <img
+                className="object-center inline-block w-2/3 h-1/2 rounded-full ring-2 ring-white"
+                src={`${host.photoUrl}`}
+                alt={`${host.username}`}
+              />
+              <span className="absolute hidden group-hover:flex -right-1 -top-2 -translate-y-full  px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                {host.username}
+              </span>
+            </Link>
           </div>
 
           <div className="flex justify-between">
