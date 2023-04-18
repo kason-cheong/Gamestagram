e2dfc65bd07367db10578c0d2ff783161a6b725b
Tiaanz <jwtian126@gmail.com>
2023-04-18 14:06:18 +1200

    fixed the main

--- client/components/AddEvent.tsx
+++ client/components/AddEvent.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react'
 import { addEvents } from '../apis/apiClientEvents'
 
 import {
-  getGames,
-  getGamesFromAPI,
   getGameByApiId,
   addGame,
 } from '../apis/apiClientGames'
@@ -33,7 +31,7 @@ export function Addevent() {
   const [selectedGameId, setSelectedGameId] = useState('')
   const [gameName, setGameName] = useState('')
 
-  const [success, setSuccess] = useState<boolean>(false)  `
+  const [success, setSuccess] = useState<boolean>(false)
   const [filteredGames, setFilteredGames] = useState<GameDB[]>(games)
   const [address, setAddress] = useState<string | undefined>('')
 
@@ -54,7 +52,7 @@ export function Addevent() {
     // getGame()
     fetchGamesFromAPI(100)
   }, [])
-  
+
   useEffect(() => {
     const filtered = games.filter((game) =>
       game.name.toLowerCase().includes(gameName.toLowerCase())
@@ -117,10 +115,6 @@ export function Addevent() {
       time: timeDb,
     }
 
-
-
-
-
     const apiGame = games.find((game) => game.apiId === selectedGameId)
     if (apiGame) {
       const newGame = {
@@ -138,144 +132,138 @@ export function Addevent() {
       } else {
         await addGame(newGame)
         await addEvents(newEvent)
-        
       }
-}
-   
+    }
 
     setSuccess(true)
     setTimeout(() => {
       navigate('/my-events')
     }, 2000)
-
- 
-
   }
   return (
     <>
       (
-        <div className="bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md mx-auto">
-            <div>
+      <div className="bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md mx-auto">
+          <div>
             {success && (
-                <p className="text-green-500 mb-5 text-center">
-                  Event updated successfully!
-                </p>
-              )}
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Add New Event
-              </h2>
+              <p className="text-green-500 mb-5 text-center">
+                Event updated successfully!
+              </p>
+            )}
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Add New Event
+            </h2>
+          </div>
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+            <div>
+              <Autocomplete
+                disablePortal
+                value={
+                  filteredGames.find((game) => game.apiId === selectedGameId) ||
+                  null
+                }
+                id="gameName"
+                options={filteredGames}
+                getOptionLabel={(game) => game.name}
+                onChange={handleGameChange}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Game Name"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                  />
+                )}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="gameName"
+              />
             </div>
-            <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+            <input type="hidden" name="remember" value="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
               <div>
-                <Autocomplete
-                  disablePortal
-                  value={
-                    filteredGames.find(
-                      (game) => game.apiId === selectedGameId
-                    ) || null
-                  }
-                  id="gameName"
-                  options={filteredGames}
-                  getOptionLabel={(game) => game.name}
-                  onChange={handleGameChange}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Game Name"
-                      variant="outlined"
-                      margin="normal"
-                      required
-                      fullWidth
-                    />
-                  )}
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="gameName"
+                <label htmlFor="eventName" className="sr-only">
+                  Event name
+                </label>
+                <input
+                  type="text"
+                  id="eventName"
+                  name="eventName"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Event Name"
                 />
               </div>
-              <input type="hidden" name="remember" value="true" />
-              <div className="rounded-md shadow-sm -space-y-px">
-                <div>
-                  <label htmlFor="eventName" className="sr-only">
-                    Event name
-                  </label>
-                  <input
-                    type="text"
-                    id="eventName"
-                    name="eventName"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Event Name"
-                  />
-                </div>
-                <br></br>
-                <div>
-                  <label htmlFor="date">Choose a date:</label>
-                  <input
-                    type="date"
-                    id="date"
-                    name="date"
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  ></input>
-                  <br></br>
-                  <label htmlFor="time">Choose a time:</label>
-                  <input
-                    type="time"
-                    id="time"
-                    name="time"
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  ></input>
-                </div>
-                <br></br>
-                <div>
-                  <label htmlFor="description">Description:</label>
-                  <textarea
-                    id="description"
-                    name="description"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Description"
-                  />
-                </div>
-                <br></br>
-                <div>
-                  <label htmlFor="location">Location:</label>
-                  <input
-                    type="text"
-                    id="autocomplete"
-                    name="location"
-                    value={address}
-                    onChange={handleAddressChange}
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Enter your address"
-                  />
-                </div>
-                <p className="text-blue-500">{address}</p>
-                <br></br>
-
-                <div>
-                  <label htmlFor="numberPpl">Number of Players:</label>
-                  <input
-                    type="text"
-                    id="numberPpl"
-                    name="numberPpl"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Number of People"
-                  />
-                </div>
-                <br></br>
-                <button
-                  type="submit"
+              <br></br>
+              <div>
+                <label htmlFor="date">Choose a date:</label>
+                <input
+                  type="date"
+                  id="date"
+                  name="date"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                >
-                  Add
-                </button>
+                ></input>
+                <br></br>
+                <label htmlFor="time">Choose a time:</label>
+                <input
+                  type="time"
+                  id="time"
+                  name="time"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                ></input>
               </div>
-            </form>
-          </div>
+              <br></br>
+              <div>
+                <label htmlFor="description">Description:</label>
+                <textarea
+                  id="description"
+                  name="description"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Description"
+                />
+              </div>
+              <br></br>
+              <div>
+                <label htmlFor="location">Location:</label>
+                <input
+                  type="text"
+                  id="autocomplete"
+                  name="location"
+                  value={address}
+                  onChange={handleAddressChange}
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your address"
+                />
+              </div>
+              <p className="text-blue-500">{address}</p>
+              <br></br>
+
+              <div>
+                <label htmlFor="numberPpl">Number of Players:</label>
+                <input
+                  type="text"
+                  id="numberPpl"
+                  name="numberPpl"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Number of People"
+                />
+              </div>
+              <br></br>
+              <button
+                type="submit"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              >
+                Add
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
       )
     </>
   )
--- client/store/useGameStore.ts
+++ client/store/useGameStore.ts
@@ -1,24 +1,24 @@
-import { create } from 'zustand'
+// import { create } from 'zustand'
 
-import type { GameDB, Game } from '../../models/Game'
-import { getGameById } from '../apis/apiClientGames'
+// import type { GameDB, Game } from '../../models/Game'
 
-interface IGame {
-  game: Game
-  fetchGame: (id: number) => void
-}
 
-export const useGameStore = create<IGame>((set) => ({
-  game: {
-    id: 0,
-    name: '',
-    description: '',
-    averagePlayTime: '',
-    playerCount: 0,
-    photoUrl: '',
-  },
-  fetchGame: async (id: number) => {
-    const game = await getGameById(id)
-    set({ game: game })
-  },
-}))
+// interface IGame {
+//   game: Game
+//   fetchGame: (id: number) => void
+// }
+
+// export const useGameStore = create<IGame>((set) => ({
+//   game: {
+//     id: 0,
+//     name: '',
+//     description: '',
+//     averagePlayTime: '',
+//     playerCount: 0,
+//     photoUrl: '',
+//   },
+//   fetchGame: async (id: number) => {
+//     const game = await getGameById(id)
+//     set({ game: game })
+//   },
+// }))
