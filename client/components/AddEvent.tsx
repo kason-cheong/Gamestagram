83ba3435c63be6e8d038b1a184dc81007eaae9f4
cade <cadefitchett1@gmail.com>
2023-04-18 12:00:04 +1200

    add event succesfull msg + changed styling

--- client/components/AddEvent.tsx
+++ client/components/AddEvent.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { addEvents } from '../apis/apiClientEvents'
-import { getGames,getGamesFromAPI } from '../apis/apiClientGames'
+import { getGames, getGamesFromAPI } from '../apis/apiClientGames'
 import { useUserStore } from '../store/useUserStore'
 import { Game } from '../../models/Game'
 import { Autocomplete, TextField } from '@mui/material'
@@ -9,13 +9,13 @@ import { useNavigate } from 'react-router-dom'
 
 export function Addevent() {
   const navigate = useNavigate()
-  const [isAdd, setAdd] = useState(false)
   const currentUser = useUserStore((state) => state.currentUser)
   const [games, setGames] = useState<Game[]>([])
   const [selectedGameId, setSelectedGameId] = useState<number | null>(null)
   const [gameName, setGameName] = useState('')
   const [filteredGames, setFilteredGames] = useState<Game[]>(games)
-  const [address, setAddress] = useState<string|undefined>('')
+  const [address, setAddress] = useState<string | undefined>('')
+  const [success, setSuccess] = useState<boolean>(false)
   const options = {
     types: ['geocode'],
     componentRestrictions: { country: 'nz' },
@@ -44,7 +44,7 @@ export function Addevent() {
 
     autocomplete.addListener('place_changed', () => {
       const place = autocomplete.getPlace()
-     
+
       setAddress(place.formatted_address)
     })
   }, [])
@@ -93,18 +93,25 @@ export function Addevent() {
       location: address,
       time: timeDb,
     }
- 
-  
+
     await addEvents(newEvent)
-    setAdd(true)
-    navigate('/my-events')
+    setSuccess(true)
+    // setAdd(true)
+    setTimeout(() => {
+      navigate('/my-events')
+    }, 2000)
   }
   return (
     <>
-      {!isAdd ? (
+      (
         <div className="bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md mx-auto">
             <div>
+            {success && (
+                <p className="text-green-500 mb-5 text-center">
+                  Event updated successfully!
+                </p>
+              )}
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 Add New Event
               </h2>
@@ -147,9 +154,10 @@ export function Addevent() {
                     name="eventName"
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="eventName"
+                    placeholder="Event Name"
                   />
                 </div>
+                <br></br>
                 <div>
                   <label htmlFor="date">Choose a date:</label>
                   <input
@@ -158,6 +166,7 @@ export function Addevent() {
                     name="date"
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   ></input>
+                  <br></br>
                   <label htmlFor="time">Choose a time:</label>
                   <input
                     type="time"
@@ -166,22 +175,20 @@ export function Addevent() {
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   ></input>
                 </div>
+                <br></br>
                 <div>
-                  <label htmlFor="description" className="sr-only">
-                    Description
-                  </label>
+                  <label htmlFor="description">Description:</label>
                   <textarea
                     id="description"
                     name="description"
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="description"
+                    placeholder="Description"
                   />
                 </div>
+                <br></br>
                 <div>
-                  <label htmlFor="location" className="sr-only">
-                    Location
-                  </label>
+                  <label htmlFor="location">Location:</label>
                   <input
                     type="text"
                     id="autocomplete"
@@ -193,21 +200,21 @@ export function Addevent() {
                     placeholder="Enter your address"
                   />
                 </div>
-                <p>{address}</p>
+                <p className="text-blue-500">{address}</p>
+                <br></br>
 
                 <div>
-                  <label htmlFor="numberPpl" className="sr-only">
-                    Number of people playing
-                  </label>
+                  <label htmlFor="numberPpl">Number of Players:</label>
                   <input
                     type="text"
                     id="numberPpl"
                     name="numberPpl"
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="numberPpl"
+                    placeholder="Number of People"
                   />
                 </div>
+                <br></br>
                 <button
                   type="submit"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
@@ -218,15 +225,7 @@ export function Addevent() {
             </form>
           </div>
         </div>
-      ) : (
-        <div className="bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md mx-auto">
-            <h2 className="text-3xl font-extrabold text-gray-900">
-              You have added
-            </h2>
-          </div>
-        </div>
-      )}
+      )
     </>
   )
 }
