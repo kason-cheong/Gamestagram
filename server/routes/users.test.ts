ef2b44da2a63df9c75253a9cc166766c506f03fd
Kurt Liang <liangke1991@gmail.com>
2023-04-19 17:05:54 +1200

    two test for users route

---
+++ server/routes/users.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest'
+
+import server from '../server'
+import * as db from '../db/users'
+
+jest.mock('../db/users')
+
+test('GET /users should return an array of users', async () => {
+  // here we should mock our db functions
+  const mockGetUsers = jest.mocked(db.getUsers)
+  mockGetUsers.mockResolvedValue([
+    {
+      username: 'Ke123',
+      email: 'maxeipk@gmail.com',
+      bio: "A new web developer with a love for board games. Driven by my passion for learning and exploring new areas, I'm always on the lookout for exciting opportunities to grow my skills. In my free time, you can often find me deep in thought, strategizing over a game of Root or Scythe",
+      photoUrl:
+        'https://rs1.huanqiucdn.cn/dp/api/files/imageDir/bfc32da8a7fae22a0bdc8af7d67379c2.jpeg',
+      id: 1,
+      dateSignUp: new Date(),
+      auth0Id: 'kjuh1kj23sd',
+    },
+  ])
+
+  const response = await request(server).get('/api/v1/users')
+
+  expect(response.status).toBe(200)
+
+  expect(response.body[0].auth0Id).toBe('kjuh1kj23sd')
+})
+
+test('GET /users/:id should return a single user', async () => {
+  // here we should mock our db functions
+  const mockGetUsers = jest.mocked(db.getUsersById)
+  mockGetUsers.mockResolvedValue([
+    {
+      username: 'Ke123',
+      email: 'maxeipk@gmail.com',
+      bio: "A new web developer with a love for board games. Driven by my passion for learning and exploring new areas, I'm always on the lookout for exciting opportunities to grow my skills. In my free time, you can often find me deep in thought, strategizing over a game of Root or Scythe",
+      photoUrl:
+        'https://rs1.huanqiucdn.cn/dp/api/files/imageDir/bfc32da8a7fae22a0bdc8af7d67379c2.jpeg',
+      id: 1,
+      dateSignUp: new Date(),
+      auth0Id: 'kjuh1kj23sd',
+    },
+  ])
+
+  const response = await request(server).get('/api/v1/users/1')
+  console.log(response.body)
+
+  expect(response.status).toBe(200)
+
+  expect(response.body[0].email.includes('maxeipk@gmail.com')).toBeTruthy()
+})
