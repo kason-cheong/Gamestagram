1d41a8dfc02edf8f440f66570ccf93c296cd1a5e
kason cheong <kasonck@yahoo.com>
2023-04-27 15:31:25 +1200

    Update purge in tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
   content: ['./client/**/*.{html,js,ts,tsx}'],
   theme: {
     extend: {
