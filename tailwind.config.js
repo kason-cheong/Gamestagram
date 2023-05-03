01b2d881d119723e6fa51e62eaca154401d366f4
kason cheong <kasonck@yahoo.com>
2023-05-03 14:10:42 +1200

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', ],
-  content: ['./client/**/*.{html,js,ts,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./client/**/*.{html,js,ts,tsx}','./client/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
